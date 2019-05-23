d865c02a56707ebeb930615077f2815c3a505f17
mma01us <mma01us@gmail.com>
2019-05-23 15:13:43 +0000

    Facebook Authentication

--- app/routes.js
+++ app/routes.js
@@ -637,6 +637,21 @@ module.exports = function(app, passport) {
         failureFlash : true // allow flash messages
     }));
     // facebook routes
+    // =====================================
+    // FACEBOOK ROUTES =====================
+    // =====================================
+    // route for facebook authentication and login
+    app.get('/auth/facebook', passport.authenticate('facebook', { 
+      scope : ['public_profile', 'email']
+    }));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
+        
     // twitter routes
 
     // =====================================
