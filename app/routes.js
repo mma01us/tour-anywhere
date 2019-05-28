2ffe7ed0f3e815ad794df2ab64a3a61a98540bac
mma01us <mma01us@gmail.com>
2019-05-28 04:10:22 +0000

    rollback

--- app/routes.js
+++ app/routes.js
@@ -684,14 +684,18 @@ module.exports = function(app, passport) {
     // =============================================================================
     // AUTHORIZE (ALREADY LOGGED IN / CONNECTING OTHER SOCIAL ACCOUNT) =============
     // =============================================================================
-    
-        // locally --------------------------------
-        app.get('/connect/local', function(req, res) {
-            res.render('connect-local.ejs', { message: req.flash('loginMessage') });
+
+    // locally --------------------------------
+        app.get("/connect/local", function(req, res) {
+            res.render("connect-local.ejs", { message: req.flash("loginMessage") });
         });
-        app.post('/connect/local', passport.authenticate('local-signup', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
+        app.post("/connect/local", passport.authenticate("local-signup", {
+            successRedirect : "/home", // redirect to the secure profile section
+            failureRedirect : "/connect/local", // redirect back to the signup page if there is an error
             failureFlash : true // allow flash messages
         }));
-    };
+
+    // facebook -------------------------------
+    // twitter --------------------------------
+    // google ---------------------------------
+};
